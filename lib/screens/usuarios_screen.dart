66bf077aad06a6ea2ebc511818e7444f8cce8c0b
JorgeGonSan <162482388+JorgeGonSan@users.noreply.github.com>
2026-03-22 18:46:37 +0100

    Añado en la tarjeta opcion de eliminar

--- lib/screens/usuarios_screen.dart
+++ lib/screens/usuarios_screen.dart
@@ -1,30 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:formflow/models/usuario.dart';
 
-class UsuariosScreen extends StatelessWidget {
+class UsuariosScreen extends StatefulWidget {
   final List<Usuario> usuarios;
 
   const UsuariosScreen({super.key, required this.usuarios});
 
   @override
+  State<UsuariosScreen> createState() => _UsuariosScreenState();
+}
+
+class _UsuariosScreenState extends State<UsuariosScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Usuarios")),
       body: ListView.builder(
-        itemCount: usuarios.length,
+        itemCount: widget.usuarios.length,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Card(
               color: Colors.grey[300],
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(16.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Nombre: ${usuarios[index].nombre}"),
-                    Text("Edad: ${usuarios[index].edad}"),
-                    Text("Mail: ${usuarios[index].mail}"),
+                    //Columna nombre+ columna avatar/borrar
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          widget.usuarios[index].nombre,
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: const Color.fromARGB(
+                                255,
+                                42,
+                                49,
+                                49,
+                              ),
+
+                              radius: 18,
+                              child: Text(
+                                widget.usuarios[index].nombre[0],
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  widget.usuarios.removeAt(index);
+                                });
+                              },
+                              icon: Icon(Icons.delete),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "${widget.usuarios[index].edad} años",
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Icon(Icons.mail),
+                        ),
+                        Text(
+                          widget.usuarios[index].mail,
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
